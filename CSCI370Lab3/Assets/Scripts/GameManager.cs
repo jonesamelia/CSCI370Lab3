40dec532701879d010244ce4c5f9549035252996
Amelia Jones <jonesas@hendrix.edu>
2023-02-21 11:41:23 -0600

    Play button scripted

--- CSCI370Lab3/Assets/Scripts/GameManager.cs
+++ CSCI370Lab3/Assets/Scripts/GameManager.cs
@@ -1,9 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance {get; private set;}
+    public GameObject curtain;
+    private bool raiseLower = false;
+    public GameObject mainScreen;
+
+    void Awake(){
+        if (Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+        Destroy(gameObject);
+    }
+    }
+
+    IEnumerator ColorLerpFunction(bool fadeout, float duration)
+    {
+        float time = 0;
+        raiseLower = true;
+        Image curtainImg = curtain.GetComponent<Image>();
+        Color startValue;
+        Color endValue;
+        if (fadeout) {
+            startValue = new Color(0, 0, 0, 0);
+            endValue = new Color(0, 0, 0, 1);
+        } else {
+            startValue = new Color(0, 0, 0, 1);
+            endValue = new Color(0, 0, 0, 0);
+        }
+        while (time < duration)
+        {
+            curtainImg.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        curtainImg.color = endValue;
+        raiseLower = false;
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        StartCoroutine(ColorLerpFunction(true, 1));
+        while (raiseLower)
+        {
+            yield return null;
+        }
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+    while(!asyncLoad.isDone)
+    {
+        yield return null;
+    }
+
+    StartCoroutine(ColorLerpFunction(false, 1));
+    }
+    
+    public void StartGame() {
+        StartCoroutine(LoadYourAsyncScene("SampleScene"));
+        mainScreen.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
